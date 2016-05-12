47b065396c1ad5a1202919362f24f02b33cc9f46
bochatayy <yannick.bochatay@meteo.fr>
2016-05-12 11:28:09 +0200

    Ajout de CommonJS et publication NPM

--- JSYG.UndoRedo.js
+++ JSYG.UndoRedo.js
@@ -3,7 +3,22 @@
 
 (function(root,factory) {
 
-    if (typeof define != "undefined" && define.amd) define("jsyg-undoredo",["jquery","jsyg-stdconstruct","jQuery.Hotkeys"],factory);
+    if (typeof module == "object" && typeof module.exports == "object") {
+      
+      module.exports = factory(
+          require("jquery"),
+          require("jsyg-stdconstruct"),
+          require("jquery.hotkeys")
+      )
+    }
+    else if (typeof define != "undefined" && define.amd) {
+      
+      define("jsyg-undoredo",[
+        "jquery",
+        "jsyg-stdconstruct",
+        "jquery.hotkeys"
+      ],factory);
+    }
     else if (typeof jQuery == "undefined") throw new Error("jQuery is needed");
     else if (typeof JSYG != "undefined" && JSYG.StdConstruct) factory(jQuery,JSYG.StdConstruct);
     else if (typeof StdConstruct != "undefined") root.UndoRedo = factory(jQuery,StdConstruct);
--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # JSYG.UndoRedo
-Undo/Redo plugin for JSYG framework (or standalone with jQuery)
+DOM Undo/Redo plugin for JSYG framework (or standalone with jQuery)
 
-[demo](http://yannickbochatay.github.io/JSYG.UndoRedo/)
+### Demo
+[http://yannickbochatay.github.io/JSYG.UndoRedo/](http://yannickbochatay.github.io/JSYG.UndoRedo/)
+
+### Installation
+```shell
+npm install jsyg-undoredo
+```
+
+### Example with webpack/babel
+```javascript
+import UndoRedo from "jsyg-undoredo"
+import $ from "jquery"
+
+let container = $('#myContainer')
+let undoRedo = new UndoRedo(container)
+
+undoRedo.enable();
+
+$('#button').on("click", () => {
+   container.find("#myDiv").width( (i,width) => width + 50 )
+   undoRedo.saveState()
+})
+
+$('#undoButton').on("click", () => undoRedo.undo() )
+
+$('#redoButton').on("click", () => undoRedo.redo() )
+
+```
